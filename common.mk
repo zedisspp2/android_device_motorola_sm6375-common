ac7e416d5425cb93c289a43d48bb6e38a0bc76f5
ZedissPp2 <voltageos687@gmail.com>
2026-06-13 09:54:50 -0300

    sm6375-common: Setup: Dolby Lunaris

--- common.mk
+++ common.mk
@@ -373,3 +373,6 @@ PRODUCT_COPY_FILES += \
 
 # Inherit from vendor blobs
 $(call inherit-product, vendor/motorola/sm6375-common/sm6375-common-vendor.mk)
+
+# Dolby
+$(call inherit-product, hardware/dolby/dolby.mk)
